5f4fe67363eba5adae41593ec5d0d7968ac74811
Rebonanza <giventheo2211@gmail.com>
2023-06-12 22:30:45 +0700

    fix socket hangup

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.timeout = 120000;
 app.use('/', indexRouter);
 app.use('/api/users', users);
 app.use('/api/transactions', transactions);
